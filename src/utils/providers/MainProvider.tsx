435a115e2fd0fe20e84f36c11753518129654d24
Edgardo Palacios <edgard07hp@gmail.com>
2024-05-13 17:44:41 -0600

    ✨Add Azure MSAL dependencies and configuration

--- src/utils/providers/MainProvider.tsx
+++ src/utils/providers/MainProvider.tsx
@@ -1,13 +1,22 @@
 // ==> Contexts
+import { MsalProvider } from "@azure/msal-react";
 import { ConfigProvider } from "@utils/providers/ConfigProvider";
+
+// Azure confguration
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "@src/authConfig.ts";
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 interface MainProviderProps {
   children: JSX.Element | JSX.Element[];
 }
 
 export const MainProvider = ({ children }: MainProviderProps) => (
-  <ConfigProvider>
-    {/* Add others providers */}
-    {children}
-  </ConfigProvider>
+  <MsalProvider instance={msalInstance}>
+    <ConfigProvider>
+      {/* Add others providers */}
+      {children}
+    </ConfigProvider>
+  </MsalProvider>
 );
