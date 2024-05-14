b9400eced2e7c68c77f67d07afbb8d49ffead611
Edgardo Palacios <edgard07hp@gmail.com>
2024-05-14 15:34:58 -0600

    💄Refactor Loading component to use styled-components

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,41 @@
-const Loading = () => {
-  return (
-    <div className="Loading">
-      <p>Loading...</p>
-    </div>
-  );
-};
+import styled from "styled-components";
+
+const Loading = () => (
+  <Wrapper>
+    <div className="loader" />
+  </Wrapper>
+);
+
+const Wrapper = styled.section`
+  position: fixed;
+  z-index: var(--z-high);
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: var(--color-tertiary);
+
+  .loader {
+    border: 4px solid var(--color-red);
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    border-right-color: transparent;
+    animation: rot 1s linear infinite;
+    box-shadow: 0px 0px 20px var(--color-red) inset;
+  }
+
+  @keyframes rot {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default Loading;
