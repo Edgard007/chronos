fdadb15b5c955b8b52276340581447191d2f15d6
Edgardo Palacios <edgard07hp@gmail.com>
2024-05-16 16:56:14 -0600

    🚧  Add AddTask page component

---
+++ src/pages/AddTask.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+
+// Components
+import Input from "@components/Input";
+import Select from "@components/Select";
+
+interface Project {
+  id: string;
+  name: string;
+}
+
+interface Client extends Project {
+  projects: Project[];
+}
+
+const info: Client[] = [
+  {
+    id: crypto.randomUUID().toString(),
+    name: "BFA",
+    projects: [
+      {
+        id: crypto.randomUUID().toString(),
+        name: "BXI",
+      },
+      {
+        id: crypto.randomUUID().toString(),
+        name: "BMO",
+      },
+    ],
+  },
+  {
+    id: crypto.randomUUID().toString(),
+    name: "PH",
+    projects: [
+      {
+        id: crypto.randomUUID().toString(),
+        name: "Código postales",
+      },
+    ],
+  },
+];
+
+const AddTask = () => {
+  // Translations
+  const { t } = useTranslation("", { keyPrefix: "add-task" });
+
+  // States
+  const [client, setClient] = useState("");
+  const [project, setProject] = useState("");
+  const [task, setTask] = useState("");
+
+  // Data
+  const [clients, _] = useState<Client[]>(info);
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  const handleClient = (val: string) => {
+    setProject("");
+    const c = clients.find((c) => c.id === val);
+    if (c) {
+      setClient(c?.id);
+      setProjects(c.projects);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Select
+        label={t("client")}
+        placeholder={t("client-placeholder") ?? ""}
+        options={(clients || []).map((c) => ({ label: c?.name, value: c?.id }))}
+        value={client}
+        onChange={(val) => handleClient(val)}
+        width="85%"
+      />
+      <Select
+        label={t("project")}
+        placeholder={t("project-placeholder") ?? ""}
+        options={(projects || []).map((p) => ({
+          label: p?.name,
+          value: p?.id,
+        }))}
+        value={project}
+        onChange={(val) => setProject(val)}
+        width="85%"
+      />
+      <Input
+        label={t("task")}
+        type="textarea"
+        onChange={(val) => setTask(val)}
+        value={task}
+        placeholder={t("task-placeholder") ?? ""}
+        width="88%"
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  margin-top: 1rem;
+`;
+
+export default AddTask;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 
 // Hook
 import useSessionContext from "@contexts/useSessionContext";
+
+// Components
+import AddTask from "./AddTask";
+
+// Assets
 import { AddIcon } from "@assets/CustomIcons";
 
 const Home = () => {
@@ -11,19 +17,22 @@ const Home = () => {
   // Translations
   const { t } = useTranslation("", { keyPrefix: "home" });
 
+  const [showAdd, setShowAdd] = useState(false);
+
   return (
     <Wrapper>
       <div className="h_header">
         <h2>{t("title", { name: stateSession?.user?.name ?? "" })}</h2>
-        <AddIcon onClick={() => console.log("HOLAAA")} />
+        <AddIcon onClick={() => setShowAdd((prev) => !prev)} />
       </div>
+      {showAdd && <AddTask />}
     </Wrapper>
   );
 };
 
 const Wrapper = styled.section`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   min-height: 100%;
