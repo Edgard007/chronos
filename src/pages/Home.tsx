f7b523c74959e2a90a095874ab003d2e69452880
Edgardo Palacios <edgard07hp@gmail.com>
2024-05-16 13:39:44 -0600

    🚧 Add welcome message with user name to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,28 +3,46 @@ import { useTranslation } from "react-i18next";
 
 // Hook
 import useSessionContext from "@contexts/useSessionContext";
+import { AddIcon } from "@assets/CustomIcons";
 
 const Home = () => {
   const { state: stateSession } = useSessionContext();
 
   // Translations
-  const { t } = useTranslation();
+  const { t } = useTranslation("", { keyPrefix: "home" });
 
   return (
     <Wrapper>
-      Hola {stateSession?.user?.name || "Invitado"}
+      <div className="h_header">
+        <h2>{t("title", { name: stateSession?.user?.name ?? "" })}</h2>
+        <AddIcon onClick={() => console.log("HOLAAA")} />
+      </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.section`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   min-height: 100%;
   gap: 1rem;
   padding: 1rem;
+
+  .h_header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    svg {
+      width: 1.5rem;
+      height: 1.5rem;
+      cursor: pointer;
+    }
+  }
 `;
 
 export default Home;
